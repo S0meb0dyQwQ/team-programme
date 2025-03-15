ad6814bc2ae342618cbb92e0ff349cb11e4da69a
S0meb0dyQwQ <somebodyqwq7@gmail.com>
2025-03-15 16:32:03 +0300

    Delete ПРОЕКТ directory
    Паша это ужас

--- ПРОЕКТ/CountElements.h
+++
@@ -1,3 +0,0 @@
-int32_t CountElements(int32_t*, int32_t);
-int32_t CountElements(double*, int32_t);
-int32_t CountElements(char*, int32_t);
--- ПРОЕКТ/FindElement.h
+++
@@ -1,3 +0,0 @@
-int32_t FindElement(int32_t, int32_t*, int32_t);
-int32_t FindElement(double, double*, int32_t);
-int32_t FindElement(char, char*, int32_t);
